46ada6295c67222267370217c5a9dfa80757d001
gowtham palepu <s2113890@ed.ac.uk>
2021-11-15 17:32:41 +0000

    Added comments

--- Assessment 4 .R
+++ Assessment 4 .R
@@ -6,84 +6,183 @@
 ## 5. eps value
 ## 6. Comments
 
-fd1d <- function(f=f , theta) {
-  eps <- 1e-7
-  f0 <- f(theta)
-  fd1 <- theta
-  for (i in 1:length(theta)) {
-    pet <- rep(0, length(theta))
-    pet[i] <- eps
-    x1 <- theta + pet
-    f1 <- f(x1)
-    fd1[i] <- (f1 - f0)/eps
-  }
-  return(fd1)
+
+
+
+
+
+## Work Group 1 - Sanika Baxi - s2159255		Ayush Oza - s2184992		Gowtham Palepu - s2113890
+
+## BFGS Optimizer
+
+## Overview
+
+##*****************************************************************************************************
+
+## The BFGS Optimizer is an iterative method for solving non-linear optimization problems. The BFGS determines the 
+## direction of descent by preconditioning the gradient with curvature information. This is achieved by gradually improving 
+## an approximation to the Hessian matrix of the loss function obtained by gradient evaluation. This algorithm is named after
+## its inventors Broyden–Fletcher–Goldfarb–Shanno (BFGS). 
+
+## Details of the BFGS Optimizer below: 
+
+## The function bfgs is used to optimize the given objective function by taking in various arguments as described below: 
+## The bfgs function - bfgs(theta, f, ..., tol = 1e-5, fscale = 1, maxit = 100)
+## theta is the vector of initial values for our optimization problem
+## f is the objective function argument. The first argument of function is a vector of optimization parameters and second
+## argument is a logical indicating if gradient is to be calculated
+## ... is for any arguments of f that are to be passed to it after the above two
+## fscale indicates a rough estimate of magnitude of f at the optimum
+## maxit is the maximum number of BFGS iterations taken by the function before stopping further optimization
+
+## The BFGS function above returns the below values upon calling it with valid arguments: 
+
+## f being the scalar value of the objective function at the minimum
+## theta is the vector of values of parameters at the minimum
+## iter denoting the number of iterations taken by the BFGS to reach minimum
+## g denoting the gradient vector at the minimum
+## H denoting the approximated Hessian matrix at the minimum
+
+## *********************************************************************************************************
+
+## Defining and calculating the finite differencing function to calculate the gradient/differentiation of the objective function 
+## It takes in the objective function and theta as its arguments and returns the value after calculating the finite difference/first order derivative
+
+fd1d <- function(f=f , theta) {          ## Declaring and defining the function fd1d with necessary arguments
+  eps <- 1e-7                            ## Using epsilon value as 1e-7 as delta for differencing
+  
+  ## Initiating the differencing with the starting point as obtained from theta
+  
+  f0 <- f(theta)                         ## Initiating f0 starting point of the function            
+  fd1 <- theta                           ## Initiating fd1 with theta vector of starting parameters
+
+  ## The below loop is to calculate the finite difference/first order derivative of the objective function f
+  ## First, we create a vector of 0s and then perturbate with the epsilon value while performing the differencing
+  ## The finite differencing formula can be stated as fd = (f1 - f0)/eps where f1 is next value of the curve obtained 
+  ## by using the theta and eps values and f0 is the current point on the function
+  
+  for (i in 1:length(theta)) {          ## Looping over the length of theta to calculate finite differences
+    pet <- rep(0, length(theta))        ## Creating a vector of 0s to perturbate with eps values 
+    pet[i] <- eps                       ## Performing perturbation
+    x1 <- theta + pet                   ## Determining/approximating to the next point on the function
+    f1 <- f(x1)                         ## Calculating function value at the next point
+    fd1[i] <- (f1 - f0)/eps             ## Calculating finite difference between new point on the function with the old point
+  } ## End of loop 
+  return(fd1)                           ## Finally returning the obtained value of the finite difference
 }
 
-bfgs <- function(theta,f,...,tol=1e-5,fscale=1,maxit=100) {
 
-  eps <- 1e-7
-  thetaold <- theta 
-  n <- length(theta)
-  B <- I <- diag(n)
-  c1 <- 0.5
+## By the end of above function, we will have successfuly calculated the first order derivative for a given objective function
+## with given theta parameters as starting points. We now use the above function in BFGS algorithm below for various approximations 
+
+## Start of BFGS algorithm taking in arguments as descibed in the overview
+
+bfgs <- function(theta,f,...,tol=1e-5,fscale=1,maxit=100) {     ## Declaring and defining the BFGS function 
+  
+  ## Since calculating Hessian matrices is complex and costly, we use the first order gradients obtained above from fd1d to approximate
+  ## our Hessian matrix. This is achieved by combining gradient information over several steps to build up an approximate to Hessian. 
+  
+  ## Here eps is again used to perform the differencing. Then we use the theta details to perform further algorithm iterations. 
+  ## c1 and c2 are the step length conditions chosen astisfying the Wolfe conditions. 
+
+  eps <- 1e-7                                       ## Using eps as 1e-07 for differencing           
+  thetaold <- theta                                 ## Using thetaold variable to store the previous value of the function. Initially given theta
+  n <- length(theta)                                ## Initializing n to be length of theta to create a diagonal matrix
+  B <- I <- diag(n)                                 ## Initializing B and I diagonal matrices of n where B is inverse Hessian matrix 
+  c1 <- 0.5                                         ## Initializing c1 and c2 step lengths chosen satisfying Wolfe conditions
   c2 <- 0.9
-  counter <- 0
+  counter <- 0                                      ## Initializing a counter to return the number of iterations for optimization 
 
-  for (i in 1:maxit) {
+  ## Now that we have all variables required in place, we start to loop over the maxit value and start the optimization process
+  
+  for (i in 1:maxit) {                              ## Start of the loop over maxit = 100 condition to stop optimizing if maxit is reached before
     
-    gradold <- fd1d(f, thetaold) 
+	## gradold initialized to store gradient of the function at previous point on the function
     
-    step <- -1 *(B %*% gradold)
+	gradold <- fd1d(f, thetaold)                    ## gradient is obatained from fd1d function above              
     
-    thetanew <- thetaold + drop(step)
+	## The Quasi-Newton step where step = -InvHessian * gradient 
+	
+    step <- -1 *(B %*% gradold)                     ## Determining the step using gradient value obtained from finite difference
     
-    #if (f(thetanew) > f(thetaold) + c1 * (t(gradold) %*% step) 
-    #  | t(fd1d(thetaold + step)) %*% step <  c2 * (t(fd1d(f, thetaold)) %*% step) ) {
-    #  
+	## Determining the next point on the function using the step obtained above 
+	
+    thetanew <- thetaold + drop(step)               ## Storing the new position in thetanew variable 
+    
+    # if (f(thetanew) > f(thetaold) + c1 * (t(gradold) %*% step) 
+    #  t(fd1d(thetaold + step)) %*% step <  c2 * (t(fd1d(f, thetaold)) %*% step) ) {
+      
     #}
+	
+	## From here all the steps below are calculated to approimate the Hessian using the gradient values. We calculate all necessary parameters 
+	## use them in performing the iterations of BGS
     
-    s <- thetanew - thetaold
+	## Calculating difference between the new and old theta values
+	
+    s <- thetanew - thetaold                       
     
+	## Calculating the new gradient with new theta as obtained from step above using fd1d function again
+    	
     gradnew <- fd1d(f, thetanew) 
 
+    ## Calculating difference between the new and old gradients
+     	
     y <- gradnew - gradold
+	
+	## Calculaitng rho for BFGS update condition where rho = transpost(s) * y
+ 
+    rho <- drop(1/(t(s) %*% y))                 ## Calculating rho value from s and y as per definition 
+	
+	## Approximating the Hessian B where B is given below as per BFGS algorithm: 
+	
+	## B = (I - rho * s * transpose(y)) * B * (I - rho*(y * transpose(s))) + (rho * s * transpose(s))
+	## Determining B as defined above using matrix multiplications as applicable
 
-    rho <- drop(1/(t(s) %*% y))
-
-    B <- ((I - rho*(s %*% t(y))) %*% B %*% (I - rho*(y %*% t(s)))) + (rho * s %*% t(s)) 
+    B <- ((I - rho*(s %*% t(y))) %*% B %*% (I - rho*(y %*% t(s)))) + (rho * s %*% t(s))    
   
-    counter = counter + 1
+    ## Updated B 
+	
+    counter = counter + 1                     ## Updating counter for each iteration
     
+	## Checking for the first Wolfe condition
+	
     if (max(abs(gradnew)) < (abs(f(thetanew))+fscale)*tol) {
       break
     }
     
+	## Updating thetaold with thetanew for next iteration 
 
     thetaold <- thetanew
     
   }
   
 
-  Hfd <- matrix(0,n,n) ## finite diference Hessian 
-  for (i in 1:n) { ## loop over parameters 
-    th1 <- thetanew; th1[i] <- th1[i] + eps ## increase th0[i] by eps 
-    g1 <- fd1d(f, th1) ## compute resulting nll 
-    Hfd[i,] <-(g1 - gradnew)/eps ## approximate second derivs 
+  ## Calculating approximate Hessian matrix 
+
+  Hfd <- matrix(0,n,n)                                ## Initiating all 0s finite difference Hessain matrix 
+  for (i in 1:n) {                                    ## Looping over the parameter length value to iterate over the matrix and calculate entries
+    th1 <- thetanew; th1[i] <- th1[i] + eps           ## Updating th1 to thetanew obtained from above BFGS update and the eps value  
+    g1 <- fd1d(f, th1)                                ## Calculating the finite difference of the function with th1 value 
+    Hfd[i,] <-(g1 - gradnew)/eps                      ## Calculating/approximating second order derivative Hessian matrix  
   }
   
-  Hfd <-0.5 * (t(Hfd) + Hfd)
+  Hfd <-0.5 * (t(Hfd) + Hfd)                          ## Obtaining the final finite difference approximated Hessian matrix
   
+  ## Returning all the calculated values as required by the BFG function as a list - function value at thetanew, thetanew, 
+  ## number of iterations taken for approximation, newgradient, Hessian 
   
-  return(list(f = f(thetanew), theta = thetanew, iter = counter, g = gradnew, H = Hfd))
+  return(list(f = f(thetanew), theta = thetanew, iter = counter, g = gradnew, H = Hfd))        ## Return list
   
 }
 
 
-## Test Code for BFGS
+## Test Code for BFGS as suggested with Rosenbrick objective function
 
-bfgs(theta = c(-1,2), f = rb, maxit = 100, tol = 1e-5)
-fd1d(f = rb, theta = c(-1,2))
+bfgs(theta = c(-1,2), f = rb, maxit = 100, tol = 1e-5)     ## Calling bfgs function with required arguments
+fd1d(f = rb, theta = c(-1,2))                              ## Calculating finite difference 
+
+## Defining Rosenbrock function
+
 
 rb <-function(theta, getg=FALSE,k=10) {
   ## Rosenbrock objective function, suitable for use by ’bfgs’
